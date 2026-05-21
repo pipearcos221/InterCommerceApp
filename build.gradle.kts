1da68f999dcb66a7ef454457223f84150bb8b9ba
Felipe Arcos <pipearcos221@gmail.com>
2026-05-21 13:25:51 -0500

    [CHORE] Detekt configuration for static code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,20 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    allRules = false
+    autoCorrect = true
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    }
 }
