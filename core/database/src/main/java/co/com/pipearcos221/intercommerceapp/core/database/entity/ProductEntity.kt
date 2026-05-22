e573b34c232e2dee4f90251d3a0029bb4235f800
Felipe Arcos <pipearcos221@gmail.com>
2026-05-21 23:37:56 -0500

    [FEAT] Create Converters, database config, and ProductDao

--- core/database/src/main/java/co/com/pipearcos221/intercommerceapp/core/database/entity/ProductEntity.kt
+++ core/database/src/main/java/co/com/pipearcos221/intercommerceapp/core/database/entity/ProductEntity.kt
@@ -16,10 +16,5 @@ data class ProductEntity(
     @ColumnInfo(name = "brand") val brand: String,
     @ColumnInfo(name = "category") val category: String,
     @ColumnInfo(name = "thumbnail") val thumbnail: String,
-    /**
-     * IMPORTANTE: Room no puede guardar List<String> directamente.
-     * Se requiere implementar un @TypeConverter en el Database o un archivo dedicado
-     * para serializar/deserializar esta lista a un String (ej. usando JSON).
-     */
     @ColumnInfo(name = "images") val images: List<String>
 )
