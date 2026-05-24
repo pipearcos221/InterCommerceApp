98520d2d316ed3bf58ea801077ce793eb438ee65
Felipe Arcos <pipearcos221@gmail.com>
2026-05-23 23:56:12 -0500

    [FEAT] Create InterCommerceNavHost

--- app/src/main/java/co/com/pipearcos221/intercommerceapp/MainActivity.kt
+++ app/src/main/java/co/com/pipearcos221/intercommerceapp/MainActivity.kt
@@ -1,23 +1,13 @@
 package co.com.pipearcos221.intercommerceapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import co.com.pipearcos221.intercommerceapp.core.ui.navigation.CatalogRoute
-import co.com.pipearcos221.intercommerceapp.core.ui.navigation.ProductDetailRoute
 import co.com.pipearcos221.intercommerceapp.core.ui.theme.InterCommerceAppTheme
-import co.com.pipearcos221.intercommerceapp.feature.catalog.ui.CatalogScreen
-import co.com.pipearcos221.intercommerceapp.feature.product_detail.presentation.ProductDetailViewModel
-import co.com.pipearcos221.intercommerceapp.feature.product_detail.ui.ProductDetailScreen
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import co.com.pipearcos221.intercommerceapp.navigation.InterCommerceNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,29 +26,5 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun InterCommerceApp() {
     val navController = rememberNavController()
-
-    NavHost(
-        navController = navController,
-        startDestination = CatalogRoute
-    ) {
-        composable<CatalogRoute> {
-            CatalogScreen(
-                viewModel = hiltViewModel(),
-                onProductClick = { productId ->
-                    Log.d("Navigation", "Navigating to Product: $productId")
-                    navController.navigate(ProductDetailRoute(productId = productId))
-                }
-            )
-        }
-
-        composable<ProductDetailRoute> {
-            val viewModel: ProductDetailViewModel = hiltViewModel()
-            val uiState by viewModel.uiState.collectAsState()
-            
-            ProductDetailScreen(
-                uiState = uiState,
-                onBackClick = { navController.popBackStack() }
-            )
-        }
-    }
+    InterCommerceNavHost(navController = navController)
 }
---
+++ app/src/main/java/co/com/pipearcos221/intercommerceapp/navigation/InterCommerceNavHost.kt
@@ -0,0 +1,46 @@
+package co.com.pipearcos221.intercommerceapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import co.com.pipearcos221.intercommerceapp.core.ui.navigation.CatalogRoute
+import co.com.pipearcos221.intercommerceapp.core.ui.navigation.ProductDetailRoute
+import co.com.pipearcos221.intercommerceapp.feature.catalog.ui.CatalogScreen
+import co.com.pipearcos221.intercommerceapp.feature.product_detail.presentation.ProductDetailViewModel
+import co.com.pipearcos221.intercommerceapp.feature.product_detail.ui.ProductDetailScreen
+
+@Composable
+fun InterCommerceNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = CatalogRoute,
+        modifier = modifier
+    ) {
+        composable<CatalogRoute> {
+            CatalogScreen(
+                viewModel = hiltViewModel(),
+                onProductClick = { productId ->
+                    navController.navigate(ProductDetailRoute(productId = productId))
+                }
+            )
+        }
+
+        composable<ProductDetailRoute> {
+            val viewModel: ProductDetailViewModel = hiltViewModel()
+            val uiState by viewModel.uiState.collectAsState()
+            
+            ProductDetailScreen(
+                uiState = uiState,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+    }
+}
